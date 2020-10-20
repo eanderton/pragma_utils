96e473b81dedc620b598e155411ee65023482431
eanderton <eric.t.anderton@gmail.com>
2020-10-19 22:43:45 -0400

    added selfish decorator

--- pragma_utils/decorators.py
+++ pragma_utils/decorators.py
@@ -14,3 +14,16 @@ def mapped_args(fn):
         fn(**fn_kwargs)
 
     return impl
+
+def selfish(*attr_names):
+    ''' Maps attributes of self on a member function to kwargs of the same name. '''
+
+    def deco(fn):
+        def impl(self, *args, **kwargs):
+            fn_kwargs = dict(kwargs)
+            for name in attr_names:
+                if name in signature(fn).parameters:
+                    fn_kwargs[name] = getattr(self, name)
+            return fn(self, *args, **fn_kwargs)
+        return impl
+    return deco
